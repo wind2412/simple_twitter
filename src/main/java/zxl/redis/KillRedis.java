b2483f6ea8c6ecd25f393dc3c81ac4c12fd29b4d
wind2412 <944797358@qq.com>
2017-04-23 10:45:44 +0800

    succeed~~

--- src/main/java/zxl/redis/KillRedis.java
+++ src/main/java/zxl/redis/KillRedis.java
@@ -6,7 +6,11 @@ public class KillRedis {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 		
-		System.out.println(Redis.call_shell("pkill -15 redis-server", false));	//kill所有redis-server	
+		if(Redis.os.startsWith("Mac")){
+			System.out.println(Redis.call_shell("pkill -15 redis-server", false));	//kill所有redis-server				
+		} else {
+			System.out.println(Redis.call_shell("tskill redis-server", false));
+		}
 		
 	}
 
--- src/main/java/zxl/redis/Redis.java
+++ src/main/java/zxl/redis/Redis.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Redis {
+	
+	public static String os = System.getProperty("os.name");
 
 	public static String call_shell(String command, boolean bg) throws IOException, InterruptedException{
 //		ProcessBuilder pb = new ProcessBuilder(command);
@@ -27,19 +29,31 @@ public class Redis {
 	}
 	
 	public static void redis_cluster() throws IOException, InterruptedException{
-		System.out.println(call_shell("/usr/local/bin/redis-server cluster_test/6382/redis.conf", true));
-		System.out.println(call_shell("/usr/local/bin/redis-server cluster_test/6379/redis.conf", true));
-		System.out.println(call_shell("/usr/local/bin/redis-server cluster_test/6380/redis.conf", true));
-		System.out.println(call_shell("/usr/local/bin/redis-server cluster_test/6381/redis.conf",  true));
-		System.out.println(call_shell("/usr/local/bin/redis-server cluster_test/6383/redis.conf", true));
-		System.out.println(call_shell("/usr/local/bin/redis-server cluster_test/6384/redis.conf", true));
-		System.out.println(call_shell("/usr/local/bin/redis-server cluster_test/6385_add/redis.conf", true));
-		System.out.println(call_shell("/usr/local/bin/redis-server cluster_test/6386_add_slave/redis.conf", true));
+		if(Redis.os.startsWith("Mac")){
+			System.out.println(call_shell("/usr/local/bin/redis-server cluster_test/6382/redis.conf", true));
+			System.out.println(call_shell("/usr/local/bin/redis-server cluster_test/6379/redis.conf", true));
+			System.out.println(call_shell("/usr/local/bin/redis-server cluster_test/6380/redis.conf", true));
+			System.out.println(call_shell("/usr/local/bin/redis-server cluster_test/6381/redis.conf",  true));
+			System.out.println(call_shell("/usr/local/bin/redis-server cluster_test/6383/redis.conf", true));
+			System.out.println(call_shell("/usr/local/bin/redis-server cluster_test/6384/redis.conf", true));
+			System.out.println(call_shell("/usr/local/bin/redis-server cluster_test/6385_add/redis.conf", true));
+			System.out.println(call_shell("/usr/local/bin/redis-server cluster_test/6386_add_slave/redis.conf", true));			
+		} else {
+			System.out.println(call_shell("\"C:\\Program Files\\Redis\\redis-server.exe\" cluster_test\\6382\\redis.conf", true));
+			System.out.println(call_shell("\"C:\\Program Files\\Redis\\redis-server.exe\" cluster_test\\6379\\redis.conf", true));
+			System.out.println(call_shell("\"C:\\Program Files\\Redis\\redis-server.exe\" cluster_test\\6380\\redis.conf", true));
+			System.out.println(call_shell("\"C:\\Program Files\\Redis\\redis-server.exe\" cluster_test\\6381\\redis.conf",  true));
+			System.out.println(call_shell("\"C:\\Program Files\\Redis\\redis-server.exe\" cluster_test\\6383\\redis.conf", true));
+			System.out.println(call_shell("\"C:\\Program Files\\Redis\\redis-server.exe\" cluster_test\\6384\\redis.conf", true));
+			System.out.println(call_shell("\"C:\\Program Files\\Redis\\redis-server.exe\" cluster_test\\6385_add\\redis.conf", true));
+			System.out.println(call_shell("\"C:\\Program Files\\Redis\\redis-server.exe\" cluster_test\\6386_add_slave\\redis.conf", true));						
+		}
 	}
 	
 	public static void main(String[] args) throws IOException, InterruptedException {
 		
 		Redis.redis_cluster();	//open redis cluster~
+//		System.out.println(System.getProperty("os.name"));
 //		System.out.println(call_shell("ls -l cluster_test", false));
 //		System.out.println(call_shell("./cluster_test/redis-trib.rb create --replicas 1 127.0.0.1:6379 127.0.0.1:6380 127.0.0.1:6381 127.0.0.1:6382 127.0.0.1:6383 127.0.0.1:6384", false));
 		
