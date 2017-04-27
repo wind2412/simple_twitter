03f69c1f29c175327b230211b1994bd8181ae0f2
wind2412 <944797358@qq.com>
2017-04-27 21:07:22 +0800

    fix CLUSTER DOWN problem because redis-server needs 2 second that can open up!! only one second cannot open the server!!! FUCK !!!

--- src/test/java/zxl/redis/ClusterTest.java
+++ src/test/java/zxl/redis/ClusterTest.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import org.junit.Test;
 
 import redis.clients.jedis.JedisCluster;
+import zxl.bean.Article;
+import zxl.bean.User;
 
 public class ClusterTest {
 	
@@ -14,7 +16,7 @@ public class ClusterTest {
 		//先要更换成为测试数据库。
 		try {
 			KillRedis.main(null);		//杀死目前的数据库进程。
-			Thread.sleep(100);			//需要等待。否则还没杀完，就又开启了。
+			Thread.sleep(100);			//需要等待。否则还没杀完，就又开启了。			//这里的等待时间竟然只需要100ms！！
 			RedisTest.main(null); 		//开启测试数据库服务器
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -23,7 +25,7 @@ public class ClusterTest {
 		}		
 		//连接数据库服务器
 		try {
-			Thread.sleep(100);		//需要等待。否则服务还没开启，客户端就去连接了，肯定错误。
+			Thread.sleep(2000);		//需要等待。否则服务还没开启，客户端就去连接了，肯定错误。		//卧槽！！原来的CLUSTER DOWN居然问题在这里！！ 这里即使等待1s也不行！！开不开！！必须要2s！！！
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
@@ -38,27 +40,25 @@ public class ClusterTest {
 
 		//调用Cluster的任一方法时候，Cluster类会初始化，由于static块在最前边，所以会自动运行，即客户端会自动连接。
 		jc = Cluster.getJC();
-		jc.set("haha", "11");
-		System.out.println(jc.get("haha"));
-//		Cluster.flush_all(); 		//清空测试数据库
-//		User zxl = new User("zhengxiaolin", "123", 20);
-//		User jxc = new User("jiangxicong", "123", 20);
-//		User ltg = new User("litiange", "123", 19);
-//		User zfy = new User("zhangfangyuan", "123", 19);
-//		User wy = new User("wangyue", "123", 20);
-//		Cluster.add_a_user(zxl);	//函数内部会自动赋给zxl一个UID
-//		Cluster.add_a_user(zxl);				//***重复添加测试***
-//		Cluster.add_a_user(jxc);
-//		Cluster.add_a_user(ltg);
-//		Cluster.add_a_user(zfy);
-//		Cluster.add_a_user(wy);
-//		Cluster.add_an_article(new Article("null --By zhengxiaolin.", zxl.getUID(), 0, 0, null));		//0参数表示并非转发
-//		Cluster.add_an_article(new Article("the second blood! --By zhengxiaolin.", zxl.getUID(), 0, 0, null));		//0参数表示并非转发
-//		Cluster.add_an_article(new Article("omoshiroi --By jiangxicong.", jxc.getUID(), 0, 0, null));
-//		Cluster.add_an_article(new Article("mouth can't hold ouch!! --By litiange.", ltg.getUID(), 0, 0, null));
-//		Cluster.add_an_article(new Article("if U can take me with the trip~ --By zhangfangyuan.", zfy.getUID(), 0, 0, null));
-//		Cluster.add_an_article(new Article("T4ks --By wangyue.", wy.getUID(), 0, 0, null));
-		
+		Cluster.flush_all(); 		//清空测试数据库
+		User zxl = new User("zhengxiaolin", "123", 20);
+		User jxc = new User("jiangxicong", "123", 20);
+		User ltg = new User("litiange", "123", 19);
+		User zfy = new User("zhangfangyuan", "123", 19);
+		User wy = new User("wangyue", "123", 20);
+		Cluster.add_a_user(zxl);	//函数内部会自动赋给zxl一个UID
+		Cluster.add_a_user(zxl);				//***重复添加测试***
+		Cluster.add_a_user(jxc);
+		Cluster.add_a_user(ltg);
+		Cluster.add_a_user(zfy);
+		Cluster.add_a_user(wy);
+		Cluster.add_an_article(new Article("null --By zhengxiaolin.", zxl.getUID(), 0, 0, null));		//0参数表示并非转发
+		Cluster.add_an_article(new Article("the second blood! --By zhengxiaolin.", zxl.getUID(), 0, 0, null));		//0参数表示并非转发
+		Cluster.add_an_article(new Article("omoshiroi --By jiangxicong.", jxc.getUID(), 0, 0, null));
+		Cluster.add_an_article(new Article("mouth can't hold ouch!! --By litiange.", ltg.getUID(), 0, 0, null));
+		Cluster.add_an_article(new Article("if U can take me with the trip~ --By zhangfangyuan.", zfy.getUID(), 0, 0, null));
+		Cluster.add_an_article(new Article("T4ks --By wangyue.", wy.getUID(), 0, 0, null));
+		Cluster.get_all_keys();
 	}
 	
 	
@@ -70,9 +70,9 @@ public class ClusterTest {
 
 	@Test
 	public void testBasic(){
-		assert jc.hget("user:1", "name") == "zhengxiaolin";
-		assert jc.hget("user:1", "pass") == "123";
-		assert jc.hget("user:1", "age") == "20";
+		assert jc.hget("user:1", "name").equals("zhengxiaolin");
+		assert jc.hget("user:1", "pass").equals("123");
+		assert jc.hget("user:1", "age").equals("20");
 	} 
 	
 }
